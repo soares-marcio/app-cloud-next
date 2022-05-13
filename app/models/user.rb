f172164308059ef7dbfbd50bfe30496d007ea0b8
Marcio Soares <biodevs@icloud.com>
2022-05-13 00:24:19 -0300

    add kaminari paginate;

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  paginates_per 10
 end
